a64cd9925e8e3e683a69f9e4b2543f4688ef5786
jadsonmarliere <jds.mdo@gmail.com>
2018-09-28 17:23:56 -0300

    Novas implementações
    Adição de novas funções
    Refinamento de lógica
    Implementação de entadas mais dinâmicas.

--- Aplicativo.py
+++ Aplicativo.py
@@ -3,24 +3,75 @@ blockchain =[]
 
 
 def obtem_ultimo_valor():
-    return blockchain[-1]
+    """Retorna o ultimo valor corrente do blockchain"""
+    if len(blockchain) < 1:
+        return None
+    else:
+        return blockchain[-1]
 
 
-def add_valor(valor_transacao, ultima_transacao=[1]):
+def add_transacao(valor_transacao, ultima_transacao=[1]):
+    """Anexa um novo valor como bloco no blockchain """
+    if ultima_transacao == None:
+        ultima_transacao = [1]
     blockchain.append([ultima_transacao, valor_transacao])
 
-def obtem_entrada_usuario():
+
+def obtem_valor_transacao():
     return float(input('Entre com o seu valor de transação:' ))
 
 
-valor=obtem_entrada_usuario()
-add_valor(valor)
+def obtem_escolha_usuario():
+    escolha=input('Sua escolha: ')
+    return escolha
 
-valor=obtem_entrada_usuario()
-add_valor(ultima_transacao=obtem_ultimo_valor(),valor_transacao=valor)
 
-valor=obtem_entrada_usuario()
-add_valor(valor, obtem_ultimo_valor())
+def imprime_blockchain():
+    # saida da lista blockchain no console
+    for block in blockchain:
+        print('Saida do Blockchain')
+        print(block)
 
-print(blockchain)
 
+def verifica_chave():
+    indice_bloco = 0
+    integridade = True
+    for block in blockchain:
+        if indice_bloco == 0:
+            indice_bloco += 1
+            continue
+        if block[0] == blockchain[indice_bloco - 1]:
+            integridade = True
+        else:
+            integridade = False
+            break
+        indice_bloco += 1
+    return integridade
+
+esperando_entrada = True
+
+while esperando_entrada:
+    print('Escolha a opção: ')
+    print('1: Adicionar uma nova transação')
+    print('2: Mostrar no console os elementos blockchain:')
+    print('h: Manipular o blockchain')
+    print('s: Sair. ')
+
+    escolha = obtem_escolha_usuario()
+
+    if escolha == '1':
+        tx_valor = obtem_valor_transacao()
+        add_transacao(tx_valor, obtem_ultimo_valor())
+    elif escolha == '2':
+        imprime_blockchain()
+    elif escolha == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif escolha == 's':
+        esperando_entrada = Falses
+    else:
+        print('Entrada inválida, pegue um valor das opções! ')
+    if not verifica_chave():
+        print('Blockchain inválido!')
+
+print('Realizado')
